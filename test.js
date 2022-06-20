6ef0918a4e9d35379550ea00fefc272b8041047f
kate4ka007 <shlemena@mail.ru>
2022-06-20 14:09:03 +0300

    feat: add answers

--- test.js
+++ test.js
@@ -15,21 +15,49 @@ function merge (left, right) {
 
 let res
 
-
-
 function bifurcateArray(arr) {
   if(arr.length <= 1) {
     return arr;
   }
-
   const half = arr.length/2;
-
   let left = arr.splice(0, half)
   let right = arr
   res = merge(bifurcateArray(left), bifurcateArray(right))
-
   return res
 }
-bifurcateArray(array)
-console.log(res)
+// bifurcateArray(array)
+// console.log(res)
 
+
+// insertion
+
+function insertion(arr) {
+  
+  for (let i = 1; i < array.length; i++) {
+    for (let j = i - 1; j > -1; j--) {
+      if(arr[j+1] < arr[j]) {
+        [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]]
+      }    
+    }    
+  }
+  return arr
+}
+
+//console.log(insertion([3,5,7,9,1,2,4,6,8]))
+
+
+//buble
+
+const sortArray = (arr) => {
+  let newArr = [...arr]
+  for (let i = 0; i < newArr.length; i++) {
+    for (let j = 0; j < newArr.length; j++) {
+      if(newArr[i] < newArr[j]) {
+        let cur = newArr[i]
+        newArr[i] = newArr[j]
+        newArr[j] = cur
+      }      
+    }    
+  } return(newArr)
+}
+console.log(sortArray([3, 2, 1, 4, 6, 5]))
